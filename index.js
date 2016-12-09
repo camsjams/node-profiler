4d66f38099dec16c313d75b348da1e03599dd711
Cameron Manavian <cameron.manavian@gmail.com>
2016-12-08 16:15:08 -0800

    Tweak formatting

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 var startTime;
 
 function start() {
-    startTime = new Date().getTime();
+	startTime = new Date().getTime();
 }
 
 function stop() {
-    var endTime = new Date().getTime(),
-        diff = (endTime - startTime);
-    console.log('\nTime taken: ' + format(diff) + '\n');
+	var endTime = new Date().getTime(),
+		diff = (endTime - startTime);
+	console.log('\nTime taken: ' + format(diff) + '\n');
 }
 
 function format(milliseconds) {
-    var hours, minutes, seconds;
-    hours = parseInt(minutes = parseInt(seconds = parseInt(milliseconds / 1000)) / 60) / 60;
-    return (hours % 60) +
-        ':' + parseInt(minutes % 60) +
-        ':' +  parseInt(seconds % 60) +
-        ((milliseconds === 0) ? '' : '.' + (milliseconds % 1000));
+	var hours, minutes, seconds;
+	hours = ~~(minutes = ~~(seconds = ~~(milliseconds / 1000)) / 60) / 60;
+	return pad(hours) + ':' +
+		   pad(minutes) + ':' +
+		   pad(seconds) +
+		   ((milliseconds === 0) ? '' : '.' + (milliseconds % 1000));
+}
+
+function pad(timeVal) {
+	timeVal = ~~(timeVal % 60);
+	return timeVal < 10 ? '0' + timeVal : timeVal;
 }
 
 module.exports = {
-    start: start,
-    stop: stop
+	start: start,
+	stop: stop
 };
